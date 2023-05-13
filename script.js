495525c0bcc87dc2db9063f53f21abb97cb0f75d
Daniel Godin <DanielEGodin@gmail.com>
2023-05-13 08:57:40 -0700

    Removed all variables, comments, and functions that I was experimenting with.  Only needed ones are left.

--- script.js
+++ script.js
@@ -1,36 +1,16 @@
 // DOM Variables:
 
-
-
 // DOM ID Variables:
-const redactedT1Header = document.getElementById('redactedT1Header');
-const redactedT1MissionsContainer = document.getElementById('redactedT1MissionsContainer');
 
 // DOM Faction Column Variables:
-const factionColumnNodeList = document.getElementById('contentContainer').childNodes; // Should have 5 child nodes:  Each faction div column.  id='"factionName"Column' with a class of 'faction-column'.
-const redactedColumn = document.getElementById('redactedColumn');
 
 // DOM Class Variables:
-const tierContainer = document.getElementsByClassName('tier-container');
 const tierHeader = document.getElementsByClassName('tier-header');
-
 
 // Events / Event Handlers
 
-redactedT1Header.addEventListener('click', toggleT1TierVisible);
-
-// Class Event Listeners:
-// function classEventListeners() {
-//   for (i = 0; i < tierHeader.length; i++) {
-//     tierHeader[i].addEventListener('click', (e) => {
-//       console.log('tier toggle is working');
-//       // Need a way to know toggle classList('hidden') to Parents next sibling.  Could possibly change the structure to make the title headers a direct sibling to tier-mission-container.
-//       let parent = e.parentElement;
-//       console.log(parent);
-//     });
-//   }
-// }
-
+// Class Event Handlers:
+// Event Handler + Function to hide/show mission container under the header that users clicks.
 for (i = 0; i < tierHeader.length; i++) {
   tierHeader[i].addEventListener('click', (e) => {
     console.log('tier toggle is working');
@@ -45,18 +25,6 @@ for (i = 0; i < tierHeader.length; i++) {
 
 
 
-// Function to show/hide list of tier missions.
-// Start with 'just' Redacted T1, then expand to all tiers
-
-function toggleT1TierVisible() {
-  // When user clicks a tier title/header, show or hide its cooresponding mission list (tier-missions-container) = Up one level, next sibling, I think.
-  redactedT1MissionsContainer.classList.toggle('hidden');
-  console.log('toggle working');
-}
-
-// function toggleTierVisible(event) {
-
-// }
 
 
 
@@ -65,4 +33,3 @@ function toggleT1TierVisible() {
 
 
 // Functions To Run On Load:
-// classEventListeners();
