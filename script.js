f110c96fb054ceb17d84caeb9ec622a84d164a4e
Daniel Godin <DanielEGodin@gmail.com>
2023-05-14 08:38:27 -0700

    checking redacted t1 missions changes a counter 0,1,2,3,etc / 7 in the header.  Next: Develop way for it to check and change the parent, parent, sibling, etc.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // DOM Variables:
 
 // DOM ID Variables:
+const redactedT1Progress = document.getElementById('redactedT1Progress');
 
 // DOM Faction Column Variables:
 
@@ -15,12 +16,12 @@ const missionCheckbox = document.getElementsByClassName('mission-checkbox');
 // Event Handler + Function to hide/show mission container under the header that users clicks.
 for (i = 0; i < tierHeader.length; i++) {
   tierHeader[i].addEventListener('click', (e) => {
-    console.log('tier toggle is working');
+    // console.log('tier toggle is working');
     // Need a way to know toggle classList('hidden') to Parents next sibling.  Could possibly change the structure to make the title headers a direct sibling to tier-mission-container.
     let parent = e.target.parentNode;
     let linkedMissionsContainer = e.target.parentNode.nextElementSibling;
-    console.log(parent);
-    console.log(linkedMissionsContainer);
+    // console.log(parent);
+    // console.log(linkedMissionsContainer);
     linkedMissionsContainer.classList.toggle('hidden');
   });
 }
@@ -28,25 +29,35 @@ for (i = 0; i < tierHeader.length; i++) {
 // Event handler every time user clicks on a mission checkbox.
 for (i = 0; i < missionCheckbox.length; i++) {
   missionCheckbox[i].addEventListener('click', (e) => {
-    console.log('clicking checkboxes is working');
-    console.log(e.target);
+    // console.log('clicking checkboxes is working');
+    // console.log(e.target);
     let parent = e.target.parentNode;
     let grandParent = e.target.parentNode.parentNode;
     let greatGreatGrandParent = e.target.parentNode.parentNode.parentNode.parentNode;
     let tierHead = greatGreatGrandParent.previousElementSibling;
     let tierStatus = tierHead.lastElementChild;
     
-    console.log(greatGreatGrandParent);
-    console.log(tierHead);
-    console.log(tierStatus);
-  
+    // console.log(greatGreatGrandParent);
+    // console.log(tierHead);
+    // console.log(tierStatus);
+  });
+
+  missionCheckbox[i].addEventListener('click', (e) => {
+    console.log('mission checkbox tier progress display working');
+    tierProgressDisplay();
   });
 }
 
 
 
-
+// Function to calculate number of boxes checked in a tier.  Automatically updates number of missions completed.  ??/7.
 function tierProgressDisplay() {
+  // Need a refresh of redactedT1Progress element.  Right now it just adds another number.
+  let status = document.querySelectorAll('#redactedT1Form input[type="checkbox"]:checked').length;
+  console.table(status);
+
+  redactedT1Progress.innerText = `${status}`;
+
 
 }
 
@@ -61,3 +72,4 @@ function tierProgressDisplay() {
 
 
 // Functions To Run On Load:
+tierProgressDisplay();
