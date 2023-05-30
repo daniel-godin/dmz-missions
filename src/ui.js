dcb10d931b84204ffd94f1aa02c4e3710f4f7c56
Daniel Godin <DanielEGodin@gmail.com>
2023-05-30 09:08:17 -0700

    index html moved to sign-in html.  temp-style and temp index html written to navigate users to sign up or dmz missions list.  temporary.  Next... log-in and sign-up functionally work and create a UID in users collection.  In emulator, then live.

--- src/ui.js
+++ src/ui.js
@@ -1,49 +1,49 @@
-// import { AuthErrorCodes } from 'firebase/auth';
+import { AuthErrorCodes } from 'firebase/auth';
 
-// export const txtEmail = document.querySelector('#txtEmail')
-// export const txtPassword = document.querySelector('#txtPassword')
+export const txtEmail = document.querySelector('#txtEmail')
+export const txtPassword = document.querySelector('#txtPassword')
 
-// export const btnLogin = document.querySelector('#btnLogin')
-// export const btnSignup = document.querySelector('#btnSignup')
-// export const btnGoogleSignUp = document.querySelector('#btnGoogleSignUp');
-// export const btnTestingDocAdd = document.querySelector('#btnTestingDocAdd');
-// export const btnTestingDocAddTwo = document.querySelector('#btnTestingDocAddTwo');
+export const btnLogin = document.querySelector('#btnLogin')
+export const btnSignup = document.querySelector('#btnSignup')
+export const btnGoogleSignUp = document.querySelector('#btnGoogleSignUp');
+export const btnTestingDocAdd = document.querySelector('#btnTestingDocAdd');
+export const btnTestingDocAddTwo = document.querySelector('#btnTestingDocAddTwo');
 
-// export const btnLogout = document.querySelector('#btnLogout')
+export const btnLogout = document.querySelector('#btnLogout')
 
-// export const divAuthState = document.querySelector('#divAuthState')
-// export const lblAuthState = document.querySelector('#lblAuthState')
+export const divAuthState = document.querySelector('#divAuthState')
+export const lblAuthState = document.querySelector('#lblAuthState')
 
-// export const divLoginError = document.querySelector('#divLoginError')
-// export const lblLoginErrorMessage = document.querySelector('#lblLoginErrorMessage')
+export const divLoginError = document.querySelector('#divLoginError')
+export const lblLoginErrorMessage = document.querySelector('#lblLoginErrorMessage')
 
-// export const showLoginForm = () => {
-//   login.style.display = 'block'
-//   app.style.display = 'none'  
-// }
+export const showLoginForm = () => {
+  login.style.display = 'block'
+  app.style.display = 'none'  
+}
 
-// export const showApp = () => {
-//   login.style.display = 'none'
-//   app.style.display = 'block'
-// }
+export const showApp = () => {
+  login.style.display = 'none'
+  app.style.display = 'block'
+}
 
-// export const hideLoginError = () => {
-//   divLoginError.style.display = 'none'
-//   lblLoginErrorMessage.innerHTML = ''
-// }
+export const hideLoginError = () => {
+  divLoginError.style.display = 'none'
+  lblLoginErrorMessage.innerHTML = ''
+}
 
-// export const showLoginError = (error) => {
-//   divLoginError.style.display = 'block'    
-//   if (error.code == AuthErrorCodes.INVALID_PASSWORD) {
-//     lblLoginErrorMessage.innerHTML = `Wrong password. Try again.`
-//   }
-//   else {
-//     lblLoginErrorMessage.innerHTML = `Error: ${error.message}`      
-//   }
-// }
+export const showLoginError = (error) => {
+  divLoginError.style.display = 'block'    
+  if (error.code == AuthErrorCodes.INVALID_PASSWORD) {
+    lblLoginErrorMessage.innerHTML = `Wrong password. Try again.`
+  }
+  else {
+    lblLoginErrorMessage.innerHTML = `Error: ${error.message}`      
+  }
+}
 
-// export const showLoginState = (user) => {
-//   lblAuthState.innerHTML = `You're logged in as ${user.displayName} (uid: ${user.uid}, email: ${user.email}) `
-// }
+export const showLoginState = (user) => {
+  lblAuthState.innerHTML = `You're logged in as ${user.displayName} (uid: ${user.uid}, email: ${user.email}) `
+}
 
-// hideLoginError()
+hideLoginError()
