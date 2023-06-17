5a451eaede2a57d549e500ef1b56a5af24dff25d
Daniel Godin <DanielEGodin@gmail.com>
2023-06-17 08:03:59 -0700

    Started data-dmz-fob-s4 object creation with data entry.

--- src/data/data-dmz-fob-s4.js
+++ src/data/data-dmz-fob-s4.js
@@ -1,5 +1,58 @@
 // This is the Call of Duty: Modern Warefare II: DMZ Foward Operating Base (FOB) Object with all data.
 
-export const dataDmzFobS4 = {
+
+// Should I break this apart into 4 different objects?  Probably...
+export const dataDmzFobS4Stash = { // Stash, Weapons Locker, Bounty Board, Communications Station
+  wallet: {
+    unlockWallet: {
+      title: "Unlock Wallet",
+      complete: false, // true/false complete/incomplete
+      factionRequirement: "Black Mous Level 2", // Amount of Faction Needed to "Unlock"
+      tasks: {
+        task1: {
+          task: "Extract 100k in Cash",
+          complete: false, // true/false complete/incomplete
+        },
+        task2: {
+          task: "",
+          complete: false, // true/false complete/incomplete
+        },
+        task3: {
+          task: "",
+          complete: false, // true/false complete/incomplete
+        },
+      },
+      walletExpansion1: {
+        title: "Wallet Expansion 1",
+        complete: false, // true/false complete/incomplete
+        factionRequirement: "White Lotus Level 3", // Amount of Faction Needed to "Unlock"
+        tasks: {
+          task1: {
+            task: "Extract 10 bottles of liquor.",
+            complete: false, // true/false complete/incomplete
+          },
+          task2: {
+            task: "Extract 6 wine bottles.",
+            complete: false, // true/false complete/incomplete
+          },
+          task3: {
+            task: "Extract 2 aged or vintage wine bottles.",
+            complete: false, // true/false complete/incomplete
+          },
+        },
+      },
+    }
+  }
+}
+
+export const dataDmzFobS4WeaponsLocker = { // Stash, Weapons Locker, Bounty Board, Communications Station
+
+}
+
+export const dataDmzFobS4BountyBoard = { // Stash, Weapons Locker, Bounty Board, Communications Station
+
+}
+
+export const dataDmzFobS4CommunicationsStation = { // Stash, Weapons Locker, Bounty Board, Communications Station
 
 }
