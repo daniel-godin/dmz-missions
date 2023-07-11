4cb1aadb1a048638edbe59543a254b77bf77c617
Daniel Godin <DanielEGodin@gmail.com>
2023-07-11 08:52:34 -0700

    Added/Changed files

---
+++ archive/save-for-now-delete-later/db-creation.js
@@ -0,0 +1,21 @@
+import { dmzMissionsS3, dataDmzStandardMissionsS4 } from './data/data-dmz-standard-missions-s4';
+
+import { dataDMZToDoMissions, dataDMZToDoFOB } from './data/data-dmz-to-do-s4';
+
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+
+// export const addDMZMissionsS3ObjToDb = async (database) => {
+
+//   // If doc does NOT exist... MAKE IT.  If it DOES exist, do nothing.
+
+//   const docRef = doc(database, 'mw-info', 'dmzMissionsS3');
+//   let docSnap = await getDoc(docRef);
+
+//   if (docSnap.exists()) {
+//     // console.log('The S3 DMZ Object Doc Exists!')
+
+//   } else {
+//     // console.log('The S3 Doc DOES NOT EXIST.  MAKING IT NOW!');
+//     await setDoc(doc(database, 'mw2-info', 'dmzMissionsS3'), dmzMissionsS3);
+//   }
+// }
---
+++ src/ui.js
@@ -0,0 +1,31 @@
+import { loadNavigation } from "./nav";
+
+export function loadPage(user) {
+  // showLoginState();
+  if (user) {
+    loadNavigation(user);
+  } else {
+    if (dmzMissionsContainer) {
+      loadNavigation();
+      // fullCreateMissionGridLoggedOut(dmzMissionsS3);
+    } else {
+      loadNavigation();
+    }
+  }
+}
+
+export const logInRequiredFunction = async () => {
+  // const logInRequiredContainer = document.querySelector('logInRequiredContainer')
+  const logInRequiredContainer = document.getElementById('loggedInConfirmContainer');
+  // const contentContainer = document.getElementById('contentContainer');
+
+  const contentContainer = document.getElementsByClassName('main-content-container');
+
+  logInRequiredContainer.style.display = 'flex';
+  contentContainer[0].style.display = 'none';
+
+  logInRequiredContainer.insertAdjacentHTML('afterbegin', `
+    <h2>This page requires an account to work correctly.  
+    Please <a href='./auth.html'>Log In</a> or <a href='./sign-up.html'>Sign Up</a>.</h2>
+  `)
+}
