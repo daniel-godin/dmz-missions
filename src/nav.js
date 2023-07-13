23d5ec52123f01c59369d8a52c82edb6227d2ed0
Daniel Godin <DanielEGodin@gmail.com>
2023-07-13 08:46:37 -0700

    Code clean up:  CSS files.  Moved auth and mission css variables into their respective css files.

--- src/nav.js
+++ src/nav.js
@@ -8,7 +8,7 @@ export const profileLinkContainer = document.getElementById('profileLinkContaine
 
 // console.log('Beginning of nav.js triggered');
 
-export async function loadNavigation(user) {
+export const loadNavigation = async (user) => {
   // Inserts HTML first thing inside of the navContainer div
   navContainer.innerHTML = ""; // "Resets" the navContainer with nothing, and then creates the nav bar depending on if a user is signed in or not.  This works, but is a little clunky.  This is a fix to the nav bar duplicating on the page after user has signed-in or signed-up, etc.
   if (user) {
@@ -32,7 +32,7 @@ export async function loadNavigation(user) {
     <header class='logo'><a href='./index.html'>DMZ-Missions</a></header>
     <nav class='nav'>
       <ul class='nav-links'>
-        <li class='nav-link'><a href='./dmz-missions.html.html'>DMZ Missions Page</a></li>
+        <li class='nav-link'><a href='./dmz-missions.html'>DMZ Missions Page</a></li>
         <li class='nav-link'><a href='./to-do.html'>To-Do Page</a></li>
         <li class='nav-link'><a href='./auth.html'>Log In</a></li>
         <li class='nav-link'><a href='./sign-up.html'>Sign Up</a></li>
