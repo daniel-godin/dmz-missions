0315d91e265381db7cb7042d063f43f23eabdbce
Daniel Godin <DanielEGodin@gmail.com>
2023-07-25 08:33:27 -0700

    added appropriate hide commands.  Need to fix buttons at end of nav menu, and to-do html page has conflicting css.

--- src/ui.js
+++ src/ui.js
@@ -54,18 +54,18 @@ const createNavigation = async (user) => {
   <div id='middleNavbarSpecialSpace'><a class='nav-link special-link event-link' href='../events/event-the-boys-diabolical.html'>Event: The Boys (July 2023)</a></div>
 
   <button id='btnMenuClosed' class='btn-drop-down-menu'>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <svg class='svg-menu' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
     </svg>
   </button>
 
   <button id='btnMenuOpen' class='btn-drop-down-menu hide'>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <svg class='svg-menu' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       <path d="M13.46,12L19,17.54V19H17.54L12,13.46L6.46,19H5V17.54L10.54,12L5,6.46V5H6.46L12,10.54L17.54,5H19V6.46L13.46,12Z" />
     </svg>
   </button>
   
-  <nav class='nav-drop-down-menu' id='navDropDownMenu'>
+  <nav id='navDropDownMenu' class='nav-drop-down-menu hide'>
     <ul class='nav-links'>
       <li class='nav-link'><a class='event-link' href='../events/event-the-boys-diabolical.html'>Event: S4 The Boys</a></li>
       <li class='nav-link'><a href='../dmz-missions.html'>DMZ Missions Page</a></li>
