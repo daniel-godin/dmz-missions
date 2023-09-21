7c8e6fd721f68e6d9b7bb6db50eda0a3516301a5
Daniel Godin <DanielEGodin@gmail.com>
2023-09-21 12:37:27 -0700

    Started using Guard Clauses. Used in FOB creation.

--- src/fob.js
+++ src/fob.js
@@ -14,40 +14,43 @@ const currentFOBDocName = 'DMZFOBS4';
 // const currentFOBSeasonNumberOfTasks = 99; // 99 IS NOT TRUE, IT'S A PLACEHOLDER
 
 onAuthStateChanged(auth, user => {
-    if (DMZFOBContainer) { // If DMZFOBContainer is true, it means the user is on the fob.html page.
-        if (user) { // IF USER IS TRUE, MEANING IF USER IS LOGGED IN
-            // Create logged in FOB grid.
-            const docRefFOBGrid = doc(db, 'users', user.uid, 'mw2-trackers', `${currentFOBDocName}`);
-            onSnapshot(docRefFOBGrid, (snapshot) => {
-                if (snapshot.exists()) {
-                    snapshot = snapshot.data();
-                    createFOBGrid(snapshot, docRefFOBGrid, user, db);
-                } else if (!snapshot.exists()) {
-                    setDoc(doc(db, 'users', user.uid, 'mw2-trackers', `${currentFOBDocName}`), currentFOBTasks);
-                }
-            })
-        } else {
-            // Create logged OUT FOB grid.
-            createFOBGrid(currentFOBTasks, undefined, undefined, undefined);
-            recommendLogInBox(DMZFOBContainer);
-        }
+    if (!DMZFOBContainer) { console.log("not on fob page"); return; };
+
+    if (!user) {
+        console.log("no user logged in, creating non-user fob grid");
+        createFOBGrid(currentFOBTasks, undefined, undefined, undefined);
+        recommendLogInBox(DMZFOBContainer);
+        console.log("no user logged in, CREATED GRID");
+        return;
     }
+
+    const docRefFOBGrid = doc(db, 'users', user.uid, 'mw2-trackers', `${currentFOBDocName}`);
+
+    // console.log(docRefFOBGrid);
+
+    onSnapshot(docRefFOBGrid, (snapshot) => {
+        if (!snapshot.exists()) { setDoc(doc(db, 'users', user.uid, 'mw2-trackers', `${currentFOBDocName}`), currentFOBTasks); }
+        let snapObj = snapshot.data();
+        createFOBGrid(snapObj, docRefFOBGrid, user, db);
+    })
+    
 })
 
 const createFOBGrid = async (obj, docRef, user, db) => {
 
+    console.log(obj);
 
-    const arrayOfPassiveMissionScreens = ["Stash", "Weapons Locker", "Bounty Board", "Communications Station", ]
-    // Move this array into the main data object later.
-    // 11 items.  11 columns.
+    // const arrayOfPassiveMissionScreens = ["Stash", "Weapons Locker", "Bounty Board", "Communications Station", ]
+    // // Move this array into the main data object later.
+    // // 11 items.  11 columns.
 
-    for (let i = 0; i < obj.arrayOfFOBSections.length && i < 15; i++) {
-        DMZFOBContainer.insertAdjacentHTML('beforeend', `
-            <div class='fob-column' data-fob-column-id='${obj.arrayOfFOBSections[i]}'>
-                <header>${obj.arrayOfFOBSections[i]}</header>
-            </div>
-        `);
-    }
+    // for (let i = 0; i < obj.arrayOfFOBSections.length && i < 15; i++) {
+    //     DMZFOBContainer.insertAdjacentHTML('beforeend', `
+    //         <div class='fob-column' data-fob-column-id='${obj.arrayOfFOBSections[i]}'>
+    //             <header>${obj.arrayOfFOBSections[i]}</header>
+    //         </div>
+    //     `);
+    // }
 
 
 
