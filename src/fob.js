204e2b6e95d6d4dc73c24cc4affd870d2bd8aae8
Daniel Godin <DanielEGodin@gmail.com>
2023-10-19 12:56:49 -0700

    Cleaned up entire FOB.js file and moved active task code to new active-tasks.js.  FOB v1 complete.  Still bugs, but fix in v2.

--- src/fob.js
+++ src/fob.js
@@ -1,6 +1,5 @@
-import { doc, getDoc, setDoc, onSnapshot, updateDoc, arrayUnion, arrayRemove, } from "firebase/firestore";
+import { doc, setDoc, onSnapshot, } from "firebase/firestore";
 import { dataS6DMZFOB, } from "./data/data-s6-dmz-fob";
-import { dataS6DMZStandardMissions } from "./data/data-s6-dmz-standard-missions";
 import { auth, db } from "./firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import { recommendLogInBox } from "./ui";
@@ -8,6 +7,8 @@ import { recommendLogInBox } from "./ui";
 
 const DMZFOBContainer = document.getElementById('DMZFOBContainer');
 const DMZFOBTabBar = document.getElementById('DMZFOBTabBar');
+const DMZFOBInformationContainer = document.getElementById('DMZFOBInformationContainer'); // DOM ID of div container for FOB Grid.
+
 
 // Change this at the beginning of each season.
 const FOBDataObject = dataS6DMZFOB;
@@ -37,8 +38,6 @@ const createFOB = (obj, docRef, user) => {
     resetFOBGrid(); // resets the innerHTML for the FOB Grid.  This is a clunky way to fix to duplication that is happening when a user clicks a checkbox, etc.
 
     createFOBDOM(obj, user); // Which one of these parameters do I not need to pass along?
-
-    // createTaskList(obj, user); // THIS IS TEST FOR NOW.
 
     createListenerEvents(obj, docRef, user);
 }
@@ -74,8 +73,6 @@ const createFOBDOM = async (obj, user) => {
         TaskContainerClass = "fob-mission-task-container"
     };
 
-    const DMZFOBInformationContainer = document.getElementById('DMZFOBInformationContainer'); // DOM ID of div container for FOB Grid.
-
     // START OF FULL DOM CREATION FROM DATA OBJECT.
     for (let i = 0; i < obj.length && i < 20; i++) { // First Loop:
 
@@ -104,9 +101,7 @@ const createFOBDOM = async (obj, user) => {
             if (subSectionTitleMinimizeStatus == 'hideBox') { subSectionTitleMinimizeStatus = 'hide' }
             if (subSectionTitleMinimizeStatus == 'showBox' || 'null') { subSectionTitleMinimizeStatus = '' }
 
-
             let DOMAttachmentPoint = document.querySelector(`[data-attachment-id="${sectionTitle}"]`);
-
             DOMAttachmentPoint.insertAdjacentHTML('beforeend', `
                 <div class='full-fob-mission-container'>
                     <h2 class='sub-section-titles' data-storage-key='${subSectionTitle}'>${subSectionTitle}</h2>
@@ -116,16 +111,17 @@ const createFOBDOM = async (obj, user) => {
 
             for (let k = 0; k < arraySecondLevel.length && k < 20; k++) { // THIRD LOOP (NESTED IN 2ND LOOP).  Loops through each Mission Object.
 
-                let missionTitleStatus;
-                let missionUnlockedStatus;
+                let missionTitleStatus = '';
+                let missionUnlockedStatus = '';
+                let missionCheckbox = '';
 
                 let missionDataObject = arraySecondLevel[k];
 
                 let title = missionDataObject.title;
                 let missionId = missionDataObject.missionID;
                 let complete = missionDataObject.complete; // Two below if (statements) change the DOM Mission Checkbox to be "checked" or "not checked".
-                    if (complete == true) { missionTitleStatus = 'mission-complete'; complete = "checked"; };
-                    if (complete == false) { missionTitleStatus = ''; complete = "" };
+                    if (complete == true) { missionTitleStatus = 'mission-complete'; missionCheckbox = "checked"; };
+                    if (complete == false) { missionTitleStatus = ''; missionCheckbox = '' };
                 let unlocked = missionDataObject.unlocked;
                 if (user) { // If no user, there's no point to having an "unlocked" change, because it can't be saved.
                     if (unlocked == true) { missionUnlockedStatus = '' };
@@ -138,19 +134,20 @@ const createFOBDOM = async (obj, user) => {
 
                 let missionDotNotation = `${i}.${sectionTitle}.${j}.${subSectionTitle}.${k}`;
 
+                let missionTitleMinimizeStatus;
+                let storageKey = localStorage.getItem(`${missionId}`) // This is using a mission specific identifier of "missionId" as the key in localStorage.  Then grabbing the value of that key, using that value to set whether a title should be minimized or not.
 
-                let missionTitleMinimizeStatus = localStorage.getItem(`${missionId}`) // This is using a mission specific identifier of "missionId" as the key in localStorage.  Then grabbing the value of that key, using that value to set whether a title should be minimized or not.
-                if (missionTitleMinimizeStatus == 'hideBox') { missionTitleMinimizeStatus = 'hide' }
-                if (missionTitleMinimizeStatus == 'showBox' || 'null') { missionTitleMinimizeStatus = '' }
+                if (storageKey == 'hideBox') { missionTitleMinimizeStatus = 'hide' }
+                if (storageKey == 'showBox' || 'null') { missionTitleMinimizeStatus = '' }
 
                 let DOMAttachmentPoint = document.querySelector(`[data-attachment-id="${subSectionTitle}"]`);
-
-
                 DOMAttachmentPoint.insertAdjacentHTML('beforeend', `
                     <div class='fob-mission-container ${missionUnlockedStatus} ${missionTitleStatus}'>
                         <div class='fob-mission-title-container'>
                             <h3 class='fob-mission-title' data-mission-id='${missionId}'>${title}</h3>
-                            <div class='fob-mission-complete-container ${userStatus}'><input type="checkbox" class='fob-mission-checkbox' data-fob-mission-id='${missionId}' data-mission-dot-notation="${missionDotNotation}" ${complete} /></div>
+                            <div class='fob-mission-complete-container ${userStatus}'>
+                                <input type="checkbox" class='fob-mission-checkbox' data-fob-mission-id='${missionId}' data-mission-dot-notation="${missionDotNotation}" ${missionCheckbox} />
+                            </div>
                         </div>
                         <div class='fob-mission-info-container ${missionTitleMinimizeStatus}'>
                             <div class='fob-mission-tasks-container' data-attachment-id="${missionId}"></div>
@@ -192,22 +189,7 @@ const createFOBDOM = async (obj, user) => {
                             </div>
                         </li>
                     </ul>
-                `)
-
-                    // Saving Commented Code.  This works.  Updating above.
-                    // DOMAttachmentPoint.insertAdjacentHTML('beforeend', `
-                    //     <ul>
-                    //         <li class="task-list" data-task-id="">
-                    //             <input type='checkbox' class='task-checkbox ${userStatus}' data-task-id='${taskId}' data-obj-notation='${missionTaskDotNotation}' ${complete} />
-                    //             <div class='mission-task'>
-                    //                 <p class='${strikeThrough}'>${task}</p>
-                    //                 <div class='progress-container ${userStatus}'>
-                    //                     <p class='${strikeThrough}'>${progressCurrent}</p><p class='${strikeThrough}'> / </p><p class='${strikeThrough}'>${progressTotal}</p>
-                    //                 </div>
-                    //             </div>
-                    //         </li>
-                    //     </ul>
-                    // `)
+                    `)
                 }
             }
         }
@@ -218,7 +200,6 @@ const createFOBDOM = async (obj, user) => {
 const createListenerEvents = async (obj, docRef, user) => { // Listener Events:  Checkboxes, Titles (minimizing, etc.), etc.
 
     // These are Listener events that don't require someone to be logged in:
-
     // Mission Titles Listener Event.  User clicks on the mission title to minimize all the data below it.  Making the data "dissapear", helps readability.
     const arrayOfMissionTitles = document.getElementsByClassName('fob-mission-title');
     for (let i = 0; i < arrayOfMissionTitles.length && i < 500; i++) {
@@ -248,39 +229,17 @@ const createListenerEvents = async (obj, docRef, user) => { // Listener Events: 
         })
     }
 
-    // ********** TEMPORARILY DISABLILNG SECTION HEADER CLICKING.  EVERYTHING I WOULD NEED TO DO HERE, I CAN DO IN THE TAB GROUP *********
-    // Main Section Titles Listener Event.  User Clicks On Title and "hides" all missions inside of it.  Improving readability.
-    // const arrayOfSectionHeaders = document.getElementsByClassName('fob-section-header');
-    // for (let i = 0; i < arrayOfSectionHeaders.length && i < 100; i++) {
-    //     arrayOfSectionHeaders[i].addEventListener('click', (e) => {
-    //         e.target.nextElementSibling.classList.toggle('hide');
-
-    //         console.log("Section Header Clicked"); // For Testing Purposes.
-            
-    //         let storageKey = e.target.dataset.storageKey;
-
-    //         // These are to keep a user's minimized preferences stored across sessions.
-    //         if (e.target.nextElementSibling.classList.contains("hide")) { localStorage.setItem(`${storageKey}`, `hideBox`); };
-    //         if (!e.target.nextElementSibling.classList.contains("hide")) { localStorage.setItem(`${storageKey}`, `showBox`); };
-    //     })
-    // }
-
     const arrayOfTabs = document.getElementsByClassName('fob-tab-bar-items');
     for (let i = 0; i < arrayOfTabs.length && i < 10; i++) {
         arrayOfTabs[i].addEventListener('click', (e) => {
-            // console.log("Tab clicked", e.target);
 
             const storageKey = e.target.dataset.storageKey;
             const sectionTarget = document.getElementById(`section${storageKey}`);
 
-            // console.log("storageKey:", storageKey, "sectionTarget", sectionTarget);
-
             if (storageKey == 'showAll') { 
-                // console.log("All Button Cicked");
                 e.target.classList.toggle('fob-tab-bar-items-hidden');
 
                 if (e.target.classList.contains('fob-tab-bar-items-hidden')) {
-                    // console.log("All Contains Hidden");
                     for (let i = 0; i < arrayOfTabs.length && i < 10; i++) {
                         arrayOfTabs[i].classList.remove('fob-tab-bar-items-hidden');
                     }
@@ -290,30 +249,20 @@ const createListenerEvents = async (obj, docRef, user) => { // Listener Events: 
 
                         let innerStorageKey = arrayOfSectionContainers[i].dataset.fobSection;
 
-                        // console.log("innerStorageKey", innerStorageKey);
-
                         localStorage.setItem(`${innerStorageKey}`, 'showBox');
-
-
-
                     }
                 }
-            
             }
 
             if (storageKey !== 'showAll') {
                 // console.log("testsssss");
                 e.target.classList.toggle('fob-tab-bar-items-hidden');
                 sectionTarget.classList.toggle('hide');
-
-                // console.log("Not all button clicked");
     
                 // These are to keep a user's minimized preferences stored across sessions.
                 if (sectionTarget.classList.contains("hide")) { localStorage.setItem(`${storageKey}`, `hideBox`); };
                 if (!sectionTarget.classList.contains("hide")) { localStorage.setItem(`${storageKey}`, `showBox`); };
-    
             }
-
         })
     }
 
@@ -472,105 +421,3 @@ const camelCase = (str) => {
     return answer.split(" ").reduce((s, c) => 
         s + (c.charAt(0).toUpperCase() + c.slice(1)));
 }
-
-// TESTING:
-
-// const createTaskList = (obj, user) => {
-//     console.log("create Task List Function Triggered");
-
-//     let userStatus;
-//     let TaskContainerClass;
-//     if (!user) { 
-//         userStatus = "hide"; 
-//         TaskContainerClass = "fob-mission-task-container-logged-out";
-//     };
-//     if (user) { 
-//         userStatus = "" 
-//         TaskContainerClass = "fob-mission-task-container"
-//     };
-
-//     obj = obj.newSetUpKey; // This is a hacky way to get around FireStore's limitations of not allow nested arrays, nor having a document start with arrays.  Basically, newSetUpKey is the only key to the data Object, and it's value is an array with all the data.
-
-//     // let DOMAttachmentPoint = document.getElementById('testCreateTaskListContainer');
-
-//     for (let i = 0; i < obj.length && i < 20; i++) { // First Loop:
-//         let arrayOfSectionTitle = Object.keys(obj[i]);
-//         let sectionTitle = arrayOfSectionTitle[0];
-//         let arrayFirstLevel = obj[i][sectionTitle];
-
-//         let sectionHeaderMinimizeStatus = localStorage.getItem(`${sectionTitle}`) // This is using a mission specific identifier of "missionId" as the key in localStorage.  Then grabbing the value of that key, using that value to set whether a title should be minimized or not.
-//         if (sectionHeaderMinimizeStatus == 'hideBox') { sectionHeaderMinimizeStatus = 'hide' }
-//         if (sectionHeaderMinimizeStatus == 'showBox' || 'null') { sectionHeaderMinimizeStatus = '' }
-
-
-//         for (let j = 0; j < arrayFirstLevel.length && j < 20; j++) { // Second Loop (NESTED IN FIRST):
-//             let arrayOfSubSectionTitles = Object.keys(arrayFirstLevel[j]);
-//             let subSectionTitle = arrayOfSubSectionTitles[0];
-//             let arraySecondLevel = arrayFirstLevel[j][subSectionTitle];
-
-//             let subSectionTitleMinimizeStatus = localStorage.getItem(`${subSectionTitle}`) // This is using a mission specific identifier of "missionId" as the key in localStorage.  Then grabbing the value of that key, using that value to set whether a title should be minimized or not.
-//             if (subSectionTitleMinimizeStatus == 'hideBox') { subSectionTitleMinimizeStatus = 'hide' }
-//             if (subSectionTitleMinimizeStatus == 'showBox' || 'null') { subSectionTitleMinimizeStatus = '' }
-
-
-//             for (let k = 0; k < arraySecondLevel.length && k < 20; k++) { // THIRD LOOP (NESTED IN 2ND LOOP).  Loops through each Mission Object.
-//                 let missionTitleStatus;
-
-//                 let missionDataObject = arraySecondLevel[k];
-
-//                 let title = missionDataObject.title;
-//                 let missionId = missionDataObject.missionID;
-//                 let complete = missionDataObject.complete; // Two below if (statements) change the DOM Mission Checkbox to be "checked" or "not checked".
-//                     if (complete == true) { missionTitleStatus = 'mission-complete'; complete = "checked"; };
-//                     if (complete == false) { missionTitleStatus = ''; complete = "" };
-//                 let unlocked = missionDataObject.unlocked;
-//                 let factionRequirement = missionDataObject.factionRequirement;
-//                 let reward = missionDataObject.reward;
-
-//                 let arrayThirdLevel = missionDataObject.tasks;
-
-//                 let missionDotNotation = `${i}.${sectionTitle}.${j}.${subSectionTitle}.${k}`;
-
-
-//                 let missionTitleMinimizeStatus = localStorage.getItem(`${missionId}`) // This is using a mission specific identifier of "missionId" as the key in localStorage.  Then grabbing the value of that key, using that value to set whether a title should be minimized or not.
-//                 if (missionTitleMinimizeStatus == 'hideBox') { missionTitleMinimizeStatus = 'hide' }
-//                 if (missionTitleMinimizeStatus == 'showBox' || 'null') { missionTitleMinimizeStatus = '' }
-
-//                 for (let p = 0; p < arrayThirdLevel.length && p < 10; p++) { // Fourth and Last Loop (NESTED IN THIRD).  Loops Through Each Mission Object's "tasks" key.  .tasks is an array of objects.
-
-//                     let taskObj = arrayThirdLevel[p];
-
-//                     let strikeThrough;
-
-//                     let task = taskObj.task;
-//                     let taskId = taskObj.taskID;
-//                     let progressCurrent = taskObj.progressCurrent;
-//                     let progressTotal = taskObj.progressTotal;
-//                     let complete = taskObj.complete; // Two below if (statements) change the DOM Task Checkbox to be "checked" or "not checked".
-//                         if (complete == true) { strikeThrough = 'strike-through'; complete = "checked"; }
-//                         if (complete == false) { strikeThrough = ''; complete = "" };
-
-//                     let missionTaskDotNotation = `${i}.${sectionTitle}.${j}.${subSectionTitle}.${k}.tasks.${p}`
-
-//                     let DOMAttachmentPoint = document.getElementById('testCreateTaskListContainer');
-
-//                     DOMAttachmentPoint.insertAdjacentHTML('beforeend', `
-//                         <div class='test-task-container'>
-//                             <label class='test-mission-task'>
-//                                 <input type='checkbox' class='task-checkbox ${userStatus}' data-task-id='${taskId}' data-obj-notation='${missionTaskDotNotation}' ${complete} />
-//                                 <span class='${strikeThrough}'>${task}</span>
-//                             </label>
-
-//                             <div class='progress-container ${userStatus}'>
-//                                 <button class='btn-task-change-amount' data-obj-notation='${missionTaskDotNotation}' data-btn-type='-' data-progress-current='${progressCurrent}' data-progress-total='${progressTotal}'>-</button>
-//                                 <p class='${strikeThrough}'>${progressCurrent}</p><p class='${strikeThrough}'> / </p><p class='${strikeThrough}'>${progressTotal}</p>
-//                                 <button class='btn-task-change-amount' data-obj-notation='${missionTaskDotNotation}' data-btn-type='+' data-progress-current='${progressCurrent}' data-progress-total='${progressTotal}'>+</button>
-//                             </div>
-//                         </div>
-                    
-//                     `)
-//                 } // End of Fourth (and final) Loop.
-//             } // End of Third Loop.
-//         } // End of Second Loop.
-//     } // End of First Loop
-// }
